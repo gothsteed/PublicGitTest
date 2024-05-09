b1e7863f9c44a087516837f344e1edaff2bc2526
hyesun0318 <162292292+hyesun0318@users.noreply.github.com>
2024-05-09 11:52:15 +0900

    새로만듬

--- PublicGitTest/src/main/java/hyesun2/Hyesun2.java
+++ PublicGitTest/src/main/java/hyesun2/Hyesun2.java
@@ -3,9 +3,7 @@ package hyesun2;
 public class Hyesun2 {
 
 	public static void main(String[] args) {
-		System.out.println("혜썬");
-		System.out.println("혜썬");
-		System.out.println("변경이발생함");
+		System.out.println("새로만듬");
 	}
 
 }
